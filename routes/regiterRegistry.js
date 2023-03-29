fdb46bc6354b33496d541baed199af6f6c3fa472
Morgan Gicheha <morgangicheha4@gmail.com>
2023-03-29 09:53:10 +0300

    update registy

--- routes/regiterRegistry.js
+++ routes/regiterRegistry.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const path = require("path");
 
 const { notifyTelegram } = require("../utils/notification/notifyTelegram");
+const filePath = path.join(__dirname, "../jsonServices/", "services.json");
 
 router.get("/", async (req, res) => {
     res.send(services);
@@ -40,7 +41,6 @@ router.post("/register", async (req, res) => {
     let jsonServices = services;
     jsonServices.services[data["name"]] = data;
 
-    const filePath = path.join(__dirname, "../jsonServices/", "services.json");
 
     // console.log(filePath);
 
@@ -52,7 +52,24 @@ router.post("/register", async (req, res) => {
 });
 
 router.put("/register", async (req, res) => {
-    res.send(":UPDATE REGISTERY SERVICE");
+    let data = req.body;
+
+    if (data.hasOwnProperty("name") === false || data["name"].length == 0 || data["data"].hasOwnProperty("port") === false || data["data"]["port"].length == 0) {
+        return res.send({ status: "091", reason: "feild 'name' and 'port' must be present" });
+    }
+ 
+    if (!services["services"][data["name"]]) {
+        return res.status(400).send({ status: "091", reason: "Service does not exist" });
+    }
+
+    let jsonServices = services;
+    jsonServices.services[data["name"]] = data["data"];
+
+    fs.writeFile(filePath, JSON.stringify(jsonServices), (err) => {
+        if (err) res.status(500).send({ status: "091", reason: JSON.stringify(err) });
+    });
+
+    res.status(200).send({status:"000", reason:"updated"});
 });
 
 router.delete("/register", async (req, res) => {
